b33ab3d912bbf53c542e616fd10bf70f6b7adfff
Sijojoseph1997 <67533489+Sijojoseph1997@users.noreply.github.com>
2020-06-28 12:49:54 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -11,8 +11,8 @@ class Config(object):
 
     # REQUIRED
     API_KEY = ""
-    OWNER_ID = "342136008" # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "JintoNS"
+    OWNER_ID = "642892679" # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "GangLeader111"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
